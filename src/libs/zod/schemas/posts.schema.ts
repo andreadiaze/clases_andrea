21cd09ab36fc2e0f56b4500e3d621cdd3f5c4ec6
Andrea Díaz <andrea.diaz@wovigroup.com>
2025-12-09 17:33:46 -0300

    updated routes

--- src/libs/zod/schemas/posts.schema.ts
+++ src/libs/zod/schemas/posts.schema.ts
@@ -1,7 +1,24 @@
-import z from 'zod';
+import * as z from 'zod';
 
+// ---------------------------
+// TYPES
+// ---------------------------
+
+export type PostsParams = z.infer<typeof postsParamsSchema>;
+export type GetPosts = z.infer<typeof getPostsSchema>;
 export type CreatePost = z.infer<typeof createPostSchema>;
+export type UpdatePost = z.infer<typeof updatePostSchema>;
 
-export const createPostSchema = z.object({
+// ---------------------------
+// SCHEMAS
+// ---------------------------
+
+export const postsParamsSchema = z.strictObject({ id: z.string() });
+
+export const getPostsSchema = z.strictObject({ limit: z.string() });
+
+export const createPostSchema = z.strictObject({
   content: z.string().min(1).max(5),
 });
+
+export const updatePostSchema = z.strictObject({ content: z.string() });
