a6cee9ce3878ebfefe6abdc07241d7df6565f139
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-12-23 18:27:33 -0300

    first component

--- web/src/app/routes/posts.route.tsx
+++ web/src/app/routes/posts.route.tsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
+
 export const PostsRoute = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <main>
       <h1>Posts Route</h1>
+
+      <h3>{counter}</h3>
+
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
     </main>
   );
 };
